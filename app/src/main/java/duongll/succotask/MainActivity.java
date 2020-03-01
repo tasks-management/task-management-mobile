bae587477608969ff890d7f9064f419ff1c073aa
Le Duong <duonglam360@gmail.com>
2020-03-01 13:03:51 +0700

    set up project

---
+++ app/src/main/java/duongll/succotask/MainActivity.java
@@ -0,0 +1,21 @@
+package duongll.succotask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickToSubmit(View view) {
+    }
+
+    public void clickToSignIn(View view) {
+    }
+}
